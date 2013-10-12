34963a603ac0cf466cb641c222a774a22f029a54
mishadoff <mishadoff@gmail.com>
2013-10-13 02:58:02 +0300

    imgur search cleanup, medium link

--- src/bulldozer/api/imgur.clj
+++ src/bulldozer/api/imgur.clj
@@ -1,13 +1,14 @@
 (ns bulldozer.api.imgur
   (:require [clj-http.client :as http]
             [clojure.data.json :as json]
-            [bulldozer.string :as s]
-            ))
+            [bulldozer.string :as s]))
 
+;; TODO NOT SYNC
 ;; TODO Failsafe implementation
-;; TODO different image sizes
-;; TODO single/two access points
-;; TODO extract to core.clj
+;; TODO Avoid repetitions
+;; TODO skip albums
+;; TODO increase search cache
+;; TODO search random
 
 (def RANDOM_ENDPOINT "https://api.imgur.com/3/gallery/random/random/")
 (def SEARCH_ENDPOINT "https://api.imgur.com/3/gallery/search/time/0?")
@@ -15,29 +16,39 @@
 (def AUTH_HEADER {:headers {"Authorization" 
                             (str "Client-ID " CLIENT_ID)}})
 
-;; TODO get rid of set?
-(def random-cache (atom #{}))
+(def random-cache (atom []))
+(def search-cache (atom {}))
 
-(defn- get-random-images-page []
+(defn- random-images []
+  "Returns one page of random images."
   (:data
    (json/read-str (:body (http/get RANDOM_ENDPOINT AUTH_HEADER))
                   :key-fn keyword)))
 
-;; TODO
 (defn- search-images [query]
-  (json/read-str (:body (http/get SEARCH_ENDPOINT 
-                                  (assoc AUTH_HEADER 
-                                    :query-params {"q" query})))
-                 :key-fn keyword))
+  "Return one page of images matches the query."
+  (:data
+   (json/read-str (:body (http/get SEARCH_ENDPOINT 
+                                   (assoc AUTH_HEADER 
+                                     :query-params {"q" query})))
+                  :key-fn keyword)))
 
 
 ;; Part of public API
 
 (defn get-image
   ([]
-     (do 
-       (if (empty? @random-cache)
-         (reset! random-cache (set (get-random-images-page))))
-       (let [e (first @random-cache)]
-         (swap! random-cache disj e) e)))
-  ([query]))
+     (if (empty? @random-cache)
+       (reset! random-cache (random-images)))
+     (let [e (last @random-cache)]
+       (swap! random-cache pop) e))
+  ([query]
+     (if (empty? (get @search-cache query []))
+       (swap! search-cache assoc query (search-images query)))
+     (let [res (get @search-cache query [])]
+       (if (empty? res) "NO IMAGE" ;; handle later
+           (let [e (last res) popped (pop res)]
+             (swap! search-cache assoc query popped) e)))))
+
+(defn link-medium [link]
+  (clojure.string/replace link #"(.png|.jpg|.gif)" "m$1"))
--- src/bulldozer/string.clj
+++ src/bulldozer/string.clj
@@ -3,7 +3,7 @@
 
 (defn strint [s p]
   "Interpolate string with parameters.
-   (strint 'http://{:s}:{:p}' {:s 'go.com' :p 123})"
+   (strint 'http://{:s}:{:p}' {:s 'google.com' :p 8080})"
   (loop [[[a b :as x] & as] (seq p) rs s]
     (if x 
       (recur as (s/replace rs (str "{" a "}") (str b))) rs)))
