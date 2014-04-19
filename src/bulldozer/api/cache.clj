bb9972d1a212c062c332f0d59ce69a246dc78c23
mishadoff <mishadoff@gmail.com>
2014-04-19 19:03:31 +0300

    Cache for paging results

--- src/bulldozer/api/cache.clj
+++ src/bulldozer/api/cache.clj
@@ -3,24 +3,36 @@
 ;; Simple stupid cache implementation based on 
 ;; clojure.lang.PersistentQueue
 
-(defn create-cache [fun]
+(defn create-cache [fun & {:keys [initpage pagesize]
+                           :or {initpage nil
+                                pagesize nil}}]
   (atom
    {:cache (clojure.lang.PersistentQueue/EMPTY)
-    :fun fun}))
+    :fun fun
+    :initpage initpage
+    :pagesize pagesize}))
 
 (defn- retrieve-from [cache]
   (let [q (:cache @cache) 
         popped (pop q)]
-    (reset! cache {:cache popped :fun (:fun @cache)})
+    (reset! cache {:cache popped
+                   :fun (:fun @cache)
+                   :initpage (:initpage @cache)
+                   :pagesize (:pagesize @cache)})
     (first q)))
 
 (defn- fill-sync [cache]
   "Fill cache synchronously"
-  (let [fun (:fun @cache) data (fun)
+  (let [fun (:fun @cache)
+        data (apply fun :page (:initpage @cache))
         conjed (if (empty? data)
                  (:cache @cache)
                  (apply conj (:cache @cache) data))]
-    (reset! cache {:cache conjed :fun fun})))
+    (reset! cache {:cache conjed
+                   :fun fun
+                   :initpage (+ (:initpage @cache)
+                                (:pagesize @cache))
+                   :pagesize (:pagesize @cache)})))
 
 (defn retrieve [cache]
   (let [q (:cache @cache)]
--- src/bulldozer/api/google.clj
+++ src/bulldozer/api/google.clj
@@ -3,21 +3,41 @@
             [clojure.data.json :as json]
             [bulldozer.cache :as cache]))
 
-(def IMAGES_API "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=clojure")
+;; 64 Images per query
 
-#_(defn get-images [page]
+(def IMAGES_API "http://ajax.googleapis.com/ajax/services/search/images?v=1.0")
+
+(defn get-images-for-page
+  [query & {:keys [page] :or {page 0}}]
   "return list of pairs [language raw_url] from gist page"
-  (->> (str GIST_PUBLIC page)
-       (http/get)
+  (->> (http/get IMAGES_API {:query-params {"q" query
+                                            "start" page}})
        (:body)
-       (json/read-str)
-       (map #(get % "files"))
-       (mapcat (fn [m]
-                 (map (fn [[k v]]
-                        [(get v "language")
-                         (get v "raw_url")]) m)))
-       (remove #(nil? (first %))) ;; we drop not detected snippets
-       (map (fn [[lang link]] [lang (slurp link)]))
-       )) 
+       (#(json/read-str % :key-fn keyword))
+       (:responseData)
+       (:results)))
 
+;;;; CACHE
 
+(def query-cache (atom {}))
+
+(defn get-image
+  "Obtain one random image or random image with
+specified keyword.
+
+Simple cache is used. First time you perform request
+it can take some time, because google returns multiple images
+per request. Result will be cached and perform instant
+response until cache is exhausted. Keep in mynd 64 is a maximum for api request.
+"
+  ([query]
+     (let [cache (get @query-cache query)]
+       (if cache
+         (cache/retrieve cache)
+         ;; not found
+         (let [cache (cache/create-cache
+                      #(get-images-for-page query)
+                      :initpage 0
+                      :pagesize 4)]
+           (swap! query-cache assoc query cache)
+           (cache/retrieve cache))))))
